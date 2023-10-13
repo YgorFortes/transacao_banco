03fefbb6c9bb6cd21023083d2a66d4a06c7af226
Ygor Fortes <ygor1888@hotmail.com>
2023-10-13 17:39:23 -0300

    Adicionado rercuso de exclusão de categoria

--- api/controller/CategoriasController.js
+++ api/controller/CategoriasController.js
@@ -118,21 +118,25 @@ class CategoriasController{
       }
 
       //Buscando categoria por id
-      const [categoria] = await db('categorias')
+      const [categoria] = await db('categorias').select([
+        'categorias.id',
+        'categorias.descricao ',
+        'usuarios.nome as usuario',
+        'usuario_id',
+      ])
       .innerJoin('usuarios', function (){
         this.on('categorias.usuario_id', '=', 'usuarios.id')
         .andOn('usuarios.id', '=', idUsuario);
       }).where('categorias.id', id);
       
-
       
       //Verificando se categoria existe
       if((!categoria)){
         return  res.status(404).send({mensagem: "Categoria não encontrada."})
       }
 
-      const resultado = await  db('categorias').update({descricao}).where('id', id);
-      if(resultado <1){
+      const resultadoAtualizacao = await  db('categorias').update({descricao}).where('id', id);
+      if(resultadoAtualizacao <1){
         return  res.status(404).send({mensagem: "Categoria não atualizada."})
       }
 
@@ -142,15 +146,46 @@ class CategoriasController{
       return res.status(500).send({mensagem: 'Servidor com problemas. Tente novamente mais tarde!'});
     }
   }
+
+  static async excluirCategoria(req, res){
+    const {id} =req.params;
+
+    try {
+      //Resgatando id de usuário pelo token
+      const idUsuario = await resgatarIdUsuarioPorToken(req);
+
+      //Buscando categoria por id
+      const [categoria] = await db('categorias').select([
+        'categorias.id',
+        'categorias.descricao ',
+        'usuarios.nome as usuario',
+        'usuario_id',
+      ])
+      .innerJoin('usuarios', function (){
+        this.on('categorias.usuario_id', '=', 'usuarios.id')
+        .andOn('usuarios.id', '=', idUsuario);
+      }).where('categorias.id', id);
+      
+
+      
+      //Verificando se categoria existe
+      if(!categoria){
+        return res.status(404).send({mensagem: "Categoria não encontrada."})
+      }
+
+      const resultadoExclusao = await db('categorias').delete().where('id', id);
+      if(resultadoExclusao <1){
+        return res.status(404).send({mensagem: "Categoria não excluída."});
+      }
+      
+      return res.status(200).send({mensagem: 'Categoria excluída com sucesso.'})
+    } catch (erro) {
+      console.log(erro);
+      return res.status(500).send({mensagem: 'Servidor com problemas. Tente novamente mais tarde!'});
+    }
+  }
 }
 
-// async function verificaCategoriaCadastrada(res, parametro, ...novosDados){
-//   const [emailCadastrado] =  db(this.nomeTabela).count(parametro).where(parametro,valorParametro);
-//   const quantidadeEmail = emailCadastrado['count(`email`)'];
-//   if(quantidadeEmail >0){
-//     return res.status(409).send({mensagem: 'Email já cadastrado. Por favor digite outro!'});
-//   }
-// }
 
 
 
